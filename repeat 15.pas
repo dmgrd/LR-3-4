22b7ebc41ff43f1b942b0f38d4a4f75ec252476c
dmgrd <117730998+dmgrd@users.noreply.github.com>
2022-12-05 14:16:38 +0300

    Add files via upload

---
+++ repeat 15.pas
@@ -0,0 +1,17 @@
+﻿program repeat15;
+var a,c,n,s,v:integer;
+begin
+  writeln('Введите число');
+  readln(a);
+  v:=1;
+  repeat 
+    c:=a mod 10;
+    a:=a div 10;
+    n:=n+1;
+    s:=s+c;
+    v:=v*c;
+  until a=0;
+  writeln('Количество цифр в числе: ',n);
+  writeln('Сумма числа ',s);
+  writeln('Произведение числа ',v);
+end.
